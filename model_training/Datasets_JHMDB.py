1457e4a8abfe813206620fd76eed06c50ae193ef
Oytun Ulutan <oytunulutan@gmail.com>
2019-12-10 16:51:42 -0800

    jhmdb

--- model_training/Datasets_JHMDB.py
+++ model_training/Datasets_JHMDB.py
@@ -102,245 +102,246 @@ class Data_JHMDB:
         print("Results for %s "% res_name)
 
 
-# during training a frame will be randomly selected so 
-# add the total no of frames as the current frame
-# I can use that number to randomly select the frame
-def get_train_list():
-    train_segments = []
-    for act in ALL_ACTIONS:
-        fname = SPLIT_INFO_FOLDER + '%s_test_split%i.txt' % (act, SPLIT_NO)
-        with open(fname) as fp:
-            vids_info = fp.readlines()
-        vids_info = [v.strip() for v in vids_info]
-        # vidname 1: 1 means training
-        # train_vids = ["%s %s 0" % (act, v.split(" ")[0]) for v in vids_info if v.split(" ")[1] == '1']
-        train_vids = []
-        for v in vids_info:
-            vidname, train_test = v.split(" ")
-            if train_test == '1':
-                vid_str = "%s %i" % (vidname, ANNOTATIONS[vidname]['nframes'])
-                train_vids.append(vid_str)
+    # during training a frame will be randomly selected so 
+    # add the total no of frames as the current frame
+    # I can use that number to randomly select the frame
+    def get_train_list(self):
+        train_segments = []
+        for act in self.ALL_ACTIONS:
+            fname = self.SPLIT_INFO_FOLDER + '%s_test_split%i.txt' % (act, self.SPLIT_NO)
+            with open(fname) as fp:
+                vids_info = fp.readlines()
+            vids_info = [v.strip() for v in vids_info]
+            # vidname 1: 1 means training
+            # train_vids = ["%s %s 0" % (act, v.split(" ")[0]) for v in vids_info if v.split(" ")[1] == '1']
+            train_vids = []
+            for v in vids_info:
+                vidname, train_test = v.split(" ")
+                if train_test == '1':
+                    vid_str = "%s %i" % (vidname, self.ANNOTATIONS[vidname]['nframes'])
+                    train_vids.append(vid_str)
 
-        train_segments.extend(train_vids)
-    return train_segments * 20
+            train_segments.extend(train_vids)
+        return train_segments * 20
 
-# during validation we will go through all frames individually. 
-def get_val_list():
-    val_segments = []
-    for act in ALL_ACTIONS:
-        fname = SPLIT_INFO_FOLDER + '%s_test_split%i.txt' % (act, SPLIT_NO)
-        with open(fname) as fp:
-            vids_info = fp.readlines()
-        vids_info = [v.strip() for v in vids_info]
-        # vidname 1: 1 means training
-        # val_vids = ["%s %s" % (act, v.split(" ")[0]) for v in vids_info if v.split(" ")[1] == '1']
-        val_vids = []
-        for v in vids_info:
-            vidname, train_test = v.split(" ")
-            if train_test == '2':
-                for ii in range(8, ANNOTATIONS[vidname]['nframes'] - 8):
-                    vid_str = "%s %i" % (vidname, ii)
-                    val_vids.append(vid_str)
+    # during validation we will go through all frames individually. 
+    def get_val_list(self):
+        val_segments = []
+        for act in self.ALL_ACTIONS:
+            fname = self.SPLIT_INFO_FOLDER + '%s_test_split%i.txt' % (act, self.SPLIT_NO)
+            with open(fname) as fp:
+                vids_info = fp.readlines()
+            vids_info = [v.strip() for v in vids_info]
+            # vidname 1: 1 means training
+            # val_vids = ["%s %s" % (act, v.split(" ")[0]) for v in vids_info if v.split(" ")[1] == '1']
+            val_vids = []
+            for v in vids_info:
+                vidname, train_test = v.split(" ")
+                if train_test == '2':
+                    for ii in range(8, self.ANNOTATIONS[vidname]['nframes'] - 8):
+                        vid_str = "%s %i" % (vidname, ii)
+                        val_vids.append(vid_str)
 
-        val_segments.extend(val_vids)
-    return val_segments
+            val_segments.extend(val_vids)
+        return val_segments
 
-## filters samples with no detected people!!!!
-def filter_no_detections(sample, labels_np, rois_np, no_det, segment_key):
-    rois_bool = tf.cast(rois_np, tf.bool)
-    return tf.reduce_any(rois_bool)
+    ## filters samples with no detected people!!!!
+    def filter_no_detections(sample, labels_np, rois_np, no_det, segment_key):
+        rois_bool = tf.cast(rois_np, tf.bool)
+        return tf.reduce_any(rois_bool)
 
-def get_data(segment_key, split):
-    
-    sample, center_frame = get_video_frames(segment_key, split)
-    labels_np, rois_np, no_det = get_labels(segment_key,split, center_frame)
-
-    return sample, labels_np, rois_np, no_det, segment_key
-
-def get_video_frames(segment_key, split):
-
-    vidname, frame_info = segment_key.split(' ')
-    action = ANNOTATIONS[vidname]['action']
-    if split == 'train':
-        # if its train frame info is total number of frames in the segments
-        center_frame = np.random.randint(low=8, high=int(frame_info)-8)
-    else:
-        center_frame = int(frame_info)
-    
-
-    vid_path = os.path.join(VIDEOS_FOLDER, action, vidname)
-    try: 
-        video = imageio.get_reader(vid_path, 'ffmpeg')
-
-        sample = np.zeros([INPUT_T, INPUT_H, INPUT_W, 3], np.uint8)
-
-        for ii in range(INPUT_T):
-            cur_frame_idx = center_frame - INPUT_T // 2 + ii
-            if cur_frame_idx < 0 or cur_frame_idx >= ANNOTATIONS[vidname]['nframes'] :
-                continue
-            else:
-                frame = video.get_data(cur_frame_idx)
-                # frame = frame[:,:,::-1] #opencv reads bgr, i3d trained with rgb
-                #reshaped = cv2.resize(frame, (INPUT_W, INPUT_H))
-                reshaped = frame
-                sample[ii,:,:,:] = reshaped
-
-        video.close()
-    except IOError: 
-        sample = np.zeros([INPUT_T, INPUT_H, INPUT_W, 3], np.uint8)
-        print("IO ERROR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1")
-
-    return sample.astype(np.float32), center_frame
-
-def get_labels(segment_key, split, center_frame):
-    vidname, frame_info = segment_key.split(' ')
-    sample_annotations = ANNOTATIONS[vidname]
-    action = sample_annotations['action']
-
-    ann_box = sample_annotations['frame_boxes'][center_frame]
-
-    detection_results_file = os.path.join(OBJECT_DETECTIONS_FOLDER, action, "%s.json" % vidname)
-    with open(detection_results_file) as fp:
-        detection_results = json.load(fp)
-    detection_boxes = detection_results['frame_objects'][center_frame]
-    detection_boxes = [detbox for detbox in detection_boxes if detbox['class_str'] == 'person']
-    
-    if split == 'train':
-        # detections = [det for det in detections if det['score'] > 0.20]
-        if len(detection_boxes) > MAX_ROIS_IN_TRAINING:
-            # they are sorted by confidence already, take top #k
-            detection_boxes = detection_boxes[:MAX_ROIS_IN_TRAINING]
-
-    labels_np, rois_np, no_det = match_annos_with_detections([ann_box], detection_boxes, action)
-
-    return labels_np, rois_np, no_det
-
-
-
-MATCHING_IOU = 0.5
-def match_annos_with_detections(annotations, detections, action):
-    # gt_boxes = []
-    # for ann in annotations:
-    #     # left, top, right, bottom
-    #     # [0.07, 0.141, 0.684, 1.0]
-    #     cur_box = ann['bbox'] 
-    #     gt_boxes.append(cur_box)
-    gt_boxes = annotations
- 
-    det_boxes = []
-    for detection in detections:
-        # top, left, bottom, right
-        # [0.07, 0.006, 0.981, 0.317]
-        # top, left, bottom, right = detection['box'] 
-        # box = left, top, right, bottom
-        box = detection['box']
-        class_label = detection['class_str']
-        det_boxes.append(box)
- 
-    no_gt = len(gt_boxes)
-    no_det = len(det_boxes)
- 
-    iou_mtx = np.zeros([no_gt, no_det])
- 
-    for gg in range(no_gt):
-        gt_box = gt_boxes[gg]
-        for dd in range(no_det):
-            dt_box = det_boxes[dd]
-            iou_mtx[gg,dd] = IoU_box(gt_box, dt_box)
- 
-    # assume less than #MAX_ROIS boxes in each image
-    if no_det > MAX_ROIS: print('MORE DETECTIONS THAN MAX ROIS!!')
-    labels_np = np.zeros([MAX_ROIS, NUM_CLASSES], np.int32)
-    rois_np = np.zeros([MAX_ROIS, 4], np.float32) # the 0th index will be used as the featmap index
- 
-    # TODO if no_gt or no_det is 0 this will give error
-    # This is fixed within functions calling this
-    if no_gt != 0 and no_det != 0:
-        max_iou_for_each_det = np.max(iou_mtx, axis=0)
-        index_for_each_det = np.argmax(iou_mtx, axis=0)
-            
-
-        for dd in range(no_det):
-            cur_max_iou = max_iou_for_each_det[dd]
-
-            
-            top, left, bottom, right = detections[dd]['box']
-
-            # # region of interest layer expects
-            # # regions of interest as lists of:
-            # # feature map index, upper left, bottom right coordinates
-            rois_np[dd,:] = [top, left, bottom, right]
-            if cur_max_iou < MATCHING_IOU:
-                labels_np[dd, -1] = 1 # bg class for softmax
-            else:
-            #matched_ann = annotations[index_for_each_det[dd]]
-                labels_np[dd, ACT_STR_TO_NO[action]] = 1
-
- 
-    return labels_np, rois_np, no_det
-
-def get_per_class_AP(results_list):
-    '''
-    results_list is a list where each
-    result = ['path' [multilabel-binary labels] [probs vector]]
-
-    returns per class_AP vector with class average precisions
-    '''
-    class_results = [{'truth':[], 'pred':[]} for _ in range(NUM_CLASSES)]
-
-    for result in results_list:
-        cur_key = result[0]
-        cur_roi_id = result[1]
-        cur_truths = result[2]
-        cur_preds = result[3]
+    def get_data(segment_key, split):
         
-        # cur_preds = np.random.uniform(size=40)
-        # cur_preds = [0 for _ in range(40)]
+        sample, center_frame = self.get_video_frames(segment_key, split)
+        labels_np, rois_np, no_det = self.get_labels(segment_key,split, center_frame)
 
-        for cc in range(NUM_CLASSES):
-            class_results[cc]['truth'].append(cur_truths[cc])
-            class_results[cc]['pred'].append(cur_preds[cc])
+        return sample, labels_np, rois_np, no_det, segment_key
 
-    ground_truth_count = []
-    class_AP = []
-    for cc in range(NUM_CLASSES):
-        y_truth = class_results[cc]['truth']
-        y_pred = class_results[cc]['pred']
-        AP = average_precision_score(y_truth, y_pred)
+    def get_video_frames(self, segment_key, split):
 
-        # print(AP)
-        # plot_pr_curve(y_truth, y_pred)
+        vidname, frame_info = segment_key.split(' ')
+        action = self.ANNOTATIONS[vidname]['action']
+        if split == 'train':
+            # if its train frame info is total number of frames in the segments
+            center_frame = np.random.randint(low=8, high=int(frame_info)-8)
+        else:
+            center_frame = int(frame_info)
+        
+
+        vid_path = os.path.join(self.VIDEOS_FOLDER, action, vidname)
+        try: 
+            video = imageio.get_reader(vid_path, 'ffmpeg')
+
+            sample = np.zeros([self.INPUT_T, self.INPUT_H, self.INPUT_W, 3], np.uint8)
+
+            for ii in range(self.INPUT_T):
+                cur_frame_idx = center_frame - self.INPUT_T // 2 + ii
+                if cur_frame_idx < 0 or cur_frame_idx >= self.ANNOTATIONS[vidname]['nframes'] :
+                    continue
+                else:
+                    frame = video.get_data(cur_frame_idx)
+                    # frame = frame[:,:,::-1] #opencv reads bgr, i3d trained with rgb
+                    #reshaped = cv2.resize(frame, (INPUT_W, INPUT_H))
+                    reshaped = frame
+                    sample[ii,:,:,:] = reshaped
+
+            video.close()
+        except IOError: 
+            sample = np.zeros([self.INPUT_T, self.INPUT_H, self.INPUT_W, 3], np.uint8)
+            print("IO ERROR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1")
+
+        return sample.astype(np.float32), center_frame
+
+    def get_labels(self, segment_key, split, center_frame):
+        vidname, frame_info = segment_key.split(' ')
+        sample_annotations = self.ANNOTATIONS[vidname]
+        action = sample_annotations['action']
+
+        ann_box = sample_annotations['frame_boxes'][center_frame]
+
+        detection_results_file = os.path.join(self.OBJECT_DETECTIONS_FOLDER, action, "%s.json" % vidname)
+        with open(detection_results_file) as fp:
+            detection_results = json.load(fp)
+        detection_boxes = detection_results['frame_objects'][center_frame]
+        detection_boxes = [detbox for detbox in detection_boxes if detbox['class_str'] == 'person']
+        
+        if split == 'train':
+            # detections = [det for det in detections if det['score'] > 0.20]
+            if len(detection_boxes) > self.MAX_ROIS_IN_TRAINING:
+                # they are sorted by confidence already, take top #k
+                detection_boxes = detection_boxes[:self.MAX_ROIS_IN_TRAINING]
+
+        labels_np, rois_np, no_det = self.match_annos_with_detections([ann_box], detection_boxes, action)
+
+        return labels_np, rois_np, no_det
+
+
+
+    
+    def match_annos_with_detections(self, annotations, detections, action):
+        # gt_boxes = []
+        # for ann in annotations:
+        #     # left, top, right, bottom
+        #     # [0.07, 0.141, 0.684, 1.0]
+        #     cur_box = ann['bbox'] 
+        #     gt_boxes.append(cur_box)
+        MATCHING_IOU = 0.5
+        gt_boxes = annotations
+    
+        det_boxes = []
+        for detection in detections:
+            # top, left, bottom, right
+            # [0.07, 0.006, 0.981, 0.317]
+            # top, left, bottom, right = detection['box'] 
+            # box = left, top, right, bottom
+            box = detection['box']
+            class_label = detection['class_str']
+            det_boxes.append(box)
+    
+        no_gt = len(gt_boxes)
+        no_det = len(det_boxes)
+    
+        iou_mtx = np.zeros([no_gt, no_det])
+    
+        for gg in range(no_gt):
+            gt_box = gt_boxes[gg]
+            for dd in range(no_det):
+                dt_box = det_boxes[dd]
+                iou_mtx[gg,dd] = IoU_box(gt_box, dt_box)
+    
+        # assume less than #MAX_ROIS boxes in each image
+        if no_det > self.MAX_ROIS: print('MORE DETECTIONS THAN MAX ROIS!!')
+        labels_np = np.zeros([self.MAX_ROIS, self.NUM_CLASSES], np.int32)
+        rois_np = np.zeros([self.MAX_ROIS, 4], np.float32) # the 0th index will be used as the featmap index
+    
+        # TODO if no_gt or no_det is 0 this will give error
+        # This is fixed within functions calling this
+        if no_gt != 0 and no_det != 0:
+            max_iou_for_each_det = np.max(iou_mtx, axis=0)
+            index_for_each_det = np.argmax(iou_mtx, axis=0)
+                
+
+            for dd in range(no_det):
+                cur_max_iou = max_iou_for_each_det[dd]
+
+                
+                top, left, bottom, right = detections[dd]['box']
+
+                # # region of interest layer expects
+                # # regions of interest as lists of:
+                # # feature map index, upper left, bottom right coordinates
+                rois_np[dd,:] = [top, left, bottom, right]
+                if cur_max_iou < MATCHING_IOU:
+                    labels_np[dd, -1] = 1 # bg class for softmax
+                else:
+                #matched_ann = annotations[index_for_each_det[dd]]
+                    labels_np[dd, self.ACT_STR_TO_NO[action]] = 1
+
+    
+        return labels_np, rois_np, no_det
+
+    def get_per_class_AP(self, results_list):
+        '''
+        results_list is a list where each
+        result = ['path' [multilabel-binary labels] [probs vector]]
+
+        returns per class_AP vector with class average precisions
+        '''
+        class_results = [{'truth':[], 'pred':[]} for _ in range(self.NUM_CLASSES)]
+
+        for result in results_list:
+            cur_key = result[0]
+            cur_roi_id = result[1]
+            cur_truths = result[2]
+            cur_preds = result[3]
+            
+            # cur_preds = np.random.uniform(size=40)
+            # cur_preds = [0 for _ in range(40)]
+
+            for cc in range(self.NUM_CLASSES):
+                class_results[cc]['truth'].append(cur_truths[cc])
+                class_results[cc]['pred'].append(cur_preds[cc])
+
+        ground_truth_count = []
+        class_AP = []
+        for cc in range(self.NUM_CLASSES):
+            y_truth = class_results[cc]['truth']
+            y_pred = class_results[cc]['pred']
+            AP = average_precision_score(y_truth, y_pred)
+
+            # print(AP)
+            # plot_pr_curve(y_truth, y_pred)
+            # import pdb; pdb.set_trace()
+
+            if np.isnan(AP): AP = 0
+
+            class_AP.append(AP)
+            ground_truth_count.append(sum(y_truth))
+            
         # import pdb; pdb.set_trace()
+        return class_AP, ground_truth_count
 
-        if np.isnan(AP): AP = 0
+    def get_class_AP_str(self, class_AP, cnt):
+        ''' Returns a printable string'''
+        ap_str = ''
+        for cc in range(self.NUM_CLASSES):
+            class_str = self.ACT_NO_TO_STR[cc][0:15] # just take the first 15 chars, some of them are too long
+            class_cnt = cnt[cc]
+            AP = class_AP[cc]
+            # AP = AP if not np.isnan(AP) else 0
+            # cur_row = '%s:    %i%% \n' %(class_str, AP*100)#class_str + ':    ' + str(tools.get_3_decimal_float(AP)) + '\n'
+            cur_row = class_str + '(%i)' % class_cnt + ':'
+            cur_row += (' ' * (25 -len(cur_row))) + '%.1f%%\n' % (AP*100.0)
+            ap_str += cur_row
+        class_avg = np.mean(class_AP)
+        # class_avg = class_avg if not np.isnan(class_avg) else 0
+        ap_str += '\n' + 'Average:' + (' '*17) + '%.1f%%\n' % (class_avg*100.0)
+        return ap_str
 
-        class_AP.append(AP)
-        ground_truth_count.append(sum(y_truth))
-        
-    # import pdb; pdb.set_trace()
-    return class_AP, ground_truth_count
-
-def get_class_AP_str(class_AP, cnt):
-    ''' Returns a printable string'''
-    ap_str = ''
-    for cc in range(NUM_CLASSES):
-        class_str = ACT_NO_TO_STR[cc][0:15] # just take the first 15 chars, some of them are too long
-        class_cnt = cnt[cc]
-        AP = class_AP[cc]
-        # AP = AP if not np.isnan(AP) else 0
-        # cur_row = '%s:    %i%% \n' %(class_str, AP*100)#class_str + ':    ' + str(tools.get_3_decimal_float(AP)) + '\n'
-        cur_row = class_str + '(%i)' % class_cnt + ':'
-        cur_row += (' ' * (25 -len(cur_row))) + '%.1f%%\n' % (AP*100.0)
-        ap_str += cur_row
-    class_avg = np.mean(class_AP)
-    # class_avg = class_avg if not np.isnan(class_avg) else 0
-    ap_str += '\n' + 'Average:' + (' '*17) + '%.1f%%\n' % (class_avg*100.0)
-    return ap_str
-
-def get_AP_str(results_list):
-    class_AP, cnt = get_per_class_AP(results_list)
-    ap_str = get_class_AP_str(class_AP, cnt)
-    return ap_str
+    def get_AP_str(self, results_list):
+        class_AP, cnt = self.get_per_class_AP(results_list)
+        ap_str = self.get_class_AP_str(class_AP, cnt)
+        return ap_str
 
 
 
