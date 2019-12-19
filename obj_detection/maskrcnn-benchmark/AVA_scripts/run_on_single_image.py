d6dfc0641f1f3bd9c79b73b7dc6da0112b49e960
Oytun Ulutan <ulutan@ece.ucsb.edu>
2019-12-19 13:18:57 -0800

    ava related stuff

---
+++ obj_detection/maskrcnn-benchmark/AVA_scripts/run_on_single_image.py
@@ -0,0 +1,21 @@
+from maskrcnn_benchmark.config import cfg
+from predictor import COCODemo
+import cv2
+
+config_file = "e2e_faster_rcnn_X_101_32x8d_FPN_1x_ava.yaml"
+
+# update the config options with the config file
+cfg.merge_from_file(config_file)
+# manual override some options
+#cfg.merge_from_list(["MODEL.DEVICE", "cpu"])
+
+coco_demo = COCODemo(
+    cfg,
+    min_image_size=800,
+    confidence_threshold=0.7,
+)
+# load image and then run prediction
+image = cv2.imread('test_img.jpg')
+predictions = coco_demo.run_on_opencv_image(image)
+cv2.imshow('Predictions', predictions)
+cv2.waitKey(0)
